fc80134104886c5fde224d121483535b3b47de0b
mnathl15 <myrealactualemail@gmail.com>
2025-11-14 18:08:25 -0500

    test

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import cors from "cors";
 import express from "express";
 
-import stimuliRouter from "./routes/stimuli";
+import stimuliRouter from "./src/routes/stimuli";
 
 import dotenv from "dotenv";
 import { NextFunction, Request, Response } from "express";
-import initDB from "./database/mongo";
-import { ErrorAdjusted } from "./types/server";
+import initDB from "./src/database/mongo";
+import { ErrorAdjusted } from "./src/types/server";
 
 dotenv.config();
 
